3aecb6dc92b2eb370de3f9f2c423a38d4bf67279
こはらちゃん <ogata_kohara@163.com>
2026-08-18 20:07:14 +0800

    Remove AXB虚拟号 from footerConfig
    Removed AXB虚拟号 configuration from footer.

--- src/config/footerConfig.ts
+++ src/config/footerConfig.ts
@@ -31,11 +31,6 @@ export const footerConfig: FooterConfig = {
 			description: "baiying800.cn",
 		},
 		{
-			name: "AXB虚拟号",
-			url: "https://axbysh.sinmeng.cn",
-			description: "axbysh.sinmeng.cn",
-		},
-		{
 			name: "和女生聊天话题",
 			url: "https://www.lee8.com/",
 			description: "www.lee8.com/",
